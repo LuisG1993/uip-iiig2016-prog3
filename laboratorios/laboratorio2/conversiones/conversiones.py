8bca86f2bb77cd960bdb0a41af1763796357b57d
azfomu <azfomu@gmail.com>
2016-09-21 21:08:42 -0500

    conversiones de monedas

---
+++ laboratorios/laboratorio2/conversiones/conversiones.py
@@ -0,0 +1,20 @@
+print("bienvenido a tu tienda de cambio  de monedas EL CHEN CHEN")
+
+
+print("¿cuanto dinero tienes?")
+
+cent  = int (input ("centavos"))
+usd = cent/100
+print("el valor de un dolar es" + str(usd))
+eur =  usd*0.89
+print("el valor de un euro es " + str(eur))
+yen =  usd * 101.57
+print("el valor de un yen es" + str(yen))
+bp =  usd*0.77
+print ("el valor de una libra esterlina es " + str(bp))
+mxn =  usd * 19.78
+print ("el valor de un peso mexicano es " + str (mxn))
+
+
+
+print("regresa pronto")
